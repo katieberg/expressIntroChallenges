2e3be7512c8dc3704a13f18dcf65ee1c24b69eca
unknown <kberg1@email.arizona.edu>
2019-05-29 14:31:43 -0700

    first part done

--- part1/server.js
+++ part1/server.js
@@ -1,14 +1,39 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var port = process.env.PORT || 8000;
 
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
 });
 
+app.get('/hello', function(req,res){
+  res.send("Hello!");
+});
+
+app.post('/create/:name', function(req,res){
+  var myName = req.params.name;
+  res.json({"id":1,"name": myName});
+});
+
+app.get('/', function(req,res){
+  res.sendFile(path.join(__dirname,'index.html'));
+});
+
+app.get("/verify/:age",function(req,res){
+  let myAge=req.params.age;
+  if(myAge<=13){
+    res.sendStatus(403)
+  }
+  else{
+    res.sendStatus(200);
+  }
+});
+
 app.use(function(req, res) {
   res.sendStatus(404);
 });
+
 
 app.listen(port, function() {
   console.log('Listening on port', port);
